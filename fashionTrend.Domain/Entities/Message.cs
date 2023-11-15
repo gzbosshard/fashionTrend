b6bf3e40a18a90cecbc0974f5bc77f36c38e7101
Gabriela Zanotto Bosshard <gzbosshard@gmail.com>
2023-11-14 21:08:45 -0300

    arrumei versão do sqlite

--- fashionTrend.Domain/Entities/Message.cs
+++ fashionTrend.Domain/Entities/Message.cs
@@ -9,7 +9,7 @@ namespace fashionTrend.Domain.Entities
     public class Message
     {
         public Guid Id { get; set; }
-        public string Sender { get; set; }
+        public string? Sender { get; set; }
         public string Receiver { get; set; }
         public string Content { get; set; }
         public DateTime Timestamp { get; set; }
